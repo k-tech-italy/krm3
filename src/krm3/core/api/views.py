9a85d4007c73eb842bf15d4dd0cf3384af4cb192
davide <d.campana@k-tech.it>
2025-05-21 10:11:03 +0100

    add logout views

--- src/krm3/core/api/views.py
+++ src/krm3/core/api/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
-from rest_framework import mixins, permissions, serializers
+from django.contrib.auth import get_user_model, logout as djlogout
+from rest_framework import mixins, permissions, serializers, status
 from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
 from rest_framework.parsers import JSONParser
@@ -47,6 +47,12 @@ class UserAPIViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericVi
     def me(self, request: Request) -> Response:
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'])
+    def logout(self, request: Request) -> Response:
+        # invalidate the session data created from the frontend if any
+        djlogout(request)
+        return Response(status=status.HTTP_200_OK)
 
 
 class ResourceSerializer(serializers.ModelSerializer):
