04be71b37fad0f95c1fcd41e97b1df227188f4d7
davide <d.campana@k-tech.it>
2025-05-21 15:01:34 +0100

    remove logout view & fix template login google

--- src/krm3/core/api/views.py
+++ src/krm3/core/api/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model, logout as djlogout
+from django.contrib.auth import get_user_model
 from rest_framework import mixins, permissions, serializers, status
 from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
@@ -45,14 +45,10 @@ class UserAPIViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericVi
 
     @action(detail=False, methods=['get'])
     def me(self, request: Request) -> Response:
+        if request.headers.get('Authorization') is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
-    
-    @action(detail=False, methods=['get'])
-    def logout(self, request: Request) -> Response:
-        # invalidate the session data created from the frontend if any
-        djlogout(request)
-        return Response(status=status.HTTP_200_OK)
 
 
 class ResourceSerializer(serializers.ModelSerializer):
