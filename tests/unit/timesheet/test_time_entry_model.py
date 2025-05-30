536a32082ec5c8f58e612a38b980233410e2c0d2
dpaolicelli-ktech <d.paolicelli@k-tech.it>
2025-05-30 17:11:04 +0200

    test(core): cover hour limits for time entry

--- tests/unit/timesheet/test_time_entry_model.py
+++ tests/unit/timesheet/test_time_entry_model.py
@@ -1,7 +1,9 @@
 import datetime
 from contextlib import nullcontext as does_not_raise
+from decimal import Decimal
 
 from django.core import exceptions
+from django.db import IntegrityError
 from krm3.core.models.timesheets import TimeEntry
 import pytest
 
@@ -96,6 +98,96 @@ class TestTimeEntry:
             )
             # NOTE: asserting the obvious to appease Ruff :^)
             assert entry.task is None
+
+    @pytest.mark.parametrize(
+        'hour_field',
+        (
+            pytest.param('day_shift_hours', id='day_shift'),
+            pytest.param('sick_hours', id='sick'),
+            pytest.param('holiday_hours', id='holiday'),
+            pytest.param('leave_hours', id='leave'),
+            pytest.param('special_leave_hours', id='special_leave'),
+            pytest.param('on_call_hours', id='on_call'),
+            pytest.param('night_shift_hours', id='night_shift'),
+            pytest.param('travel_hours', id='travel'),
+            pytest.param('rest_hours', id='rest'),
+        ),
+    )
+    def test_rejects_negative_hours(self, hour_field):
+        resource = ResourceFactory()
+        time_logged = {'day_shift_hours': 0} | {hour_field: -1}
+        with pytest.raises(IntegrityError):
+            TimeEntryFactory(
+                task=(
+                    TaskFactory(
+                        project=ProjectFactory(start_date=datetime.date(2020, 1, 1)),
+                        resource=resource,
+                        start_date=datetime.date(2020, 1, 1),
+                    )
+                    if hour_field in ('day_shift_hours', 'night_shift_hours', 'travel_hours', 'on_call_hours')
+                    else None
+                ),
+                special_leave_reason=SpecialLeaveReasonFactory() if hour_field == 'special_leave_hours' else None,
+                **time_logged,
+            )
+
+    @pytest.mark.parametrize(
+        ('hour_field', 'expected_exception'),
+        (
+            pytest.param('day_shift_hours', exceptions.ValidationError, id='day_shift'),
+            pytest.param('sick_hours', exceptions.ValidationError, id='sick'),
+            pytest.param('holiday_hours', exceptions.ValidationError, id='holiday'),
+            pytest.param('leave_hours', exceptions.ValidationError, id='leave'),
+            pytest.param('special_leave_hours', exceptions.ValidationError, id='special_leave'),
+            pytest.param('on_call_hours', IntegrityError, id='on_call'),
+            pytest.param('night_shift_hours', exceptions.ValidationError, id='night_shift'),
+            pytest.param('travel_hours', exceptions.ValidationError, id='travel'),
+            pytest.param('rest_hours', exceptions.ValidationError, id='rest'),
+        ),
+    )
+    def test_rejects_too_many_hours(self, hour_field, expected_exception):
+        resource = ResourceFactory()
+        time_logged = {'day_shift_hours': 0} | {hour_field: 25}
+        # NOTE: this should raise IntegrityError, but we're validating hour totals before saving
+        # On-call does not affect hour totals, though, so it will raise IntegrityError
+        with pytest.raises(expected_exception):
+            TimeEntryFactory(
+                date=datetime.date(2024, 1, 1),
+                task=(
+                    TaskFactory(
+                        project=ProjectFactory(start_date=datetime.date(2020, 1, 1)),
+                        resource=resource,
+                        start_date=datetime.date(2020, 1, 1),
+                    )
+                    if hour_field in ('day_shift_hours', 'night_shift_hours', 'travel_hours', 'on_call_hours')
+                    else None
+                ),
+                special_leave_reason=SpecialLeaveReasonFactory() if hour_field == 'special_leave_hours' else None,
+                **time_logged,
+            )
+
+    def test_rejects_too_many_day_shift_hours(self):
+        project = ProjectFactory(start_date=datetime.date(2020, 1, 1))
+        resource = ResourceFactory()
+        with pytest.raises(IntegrityError):
+            TimeEntryFactory(
+                date=datetime.date(2024, 1, 1),
+                resource=resource,
+                task=TaskFactory(project=project, resource=resource, start_date=datetime.date(2020, 1, 1)),
+                day_shift_hours=Decimal(16.25),
+            )
+
+    def test_rejects_too_many_night_shift_hours(self):
+        project = ProjectFactory(start_date=datetime.date(2020, 1, 1))
+        resource = ResourceFactory()
+        with pytest.raises(IntegrityError):
+            TimeEntryFactory(
+                date=datetime.date(2024, 1, 1),
+                resource=resource,
+                task=TaskFactory(project=project, resource=resource, start_date=datetime.date(2020, 1, 1)),
+                day_shift_hours=0,
+                night_shift_hours=Decimal(8.25),
+            )
 
     def test_is_saved_without_hours_logged(self):
         """Valid edge case: 0 total hours on a task."""
