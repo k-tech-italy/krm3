7cd9c3d5cbbed26f083f636db6b27f48612ed076
DavideCamp <d.campana@k-tech.it>
2025-06-24 15:18:54 +0200

    test dnd - wip

--- tests/integration/test_fe_quickadd3.py
+++ tests/integration/test_fe_quickadd3.py
@@ -6,6 +6,7 @@ import pytest
 from freezegun import freeze_time
 from testutils.factories import TaskFactory
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 
 if typing.TYPE_CHECKING:
     from testutils.selenium import AppTestBrowser
@@ -17,8 +18,7 @@ if typing.TYPE_CHECKING:
 def test_timesheet_quick_add(browser: 'AppTestBrowser', regular_user, resource_factory):
     resource = resource_factory(user=regular_user)
 
-    def crea_task():
-        TaskFactory(
+    TaskFactory(
         resource=resource,
         start_date=datetime.date(2025, 6, 1),
         end_date=datetime.date(2025, 6, 30),
@@ -29,19 +29,21 @@ def test_timesheet_quick_add(browser: 'AppTestBrowser', regular_user, resource_f
     browser.click('[href*="/timesheet"]')
     time.sleep(2)
 
-    # Seleziona il giorno corrente (dipende dal markup)
-    #today_str = "Jun 20
-    # "
-    #browser.click(f'//td[contains(text(), "{today_str}")]')  # esempio con calendario
+    element = browser.driver.find_element(By.XPATH, '//div[starts-with(@id, "draggable-Thu Jun 19 2025")]')
+    ActionChains(browser.driver).click_and_hold(element).move_by_offset(-1, 0).release().perform()
 
-    element = driver.find_element(By.XPATH, '//div[starts-with(@id, "draggable-Thu Jun 19 2025")]')
-    element.click()
-    breakpoint()
+    browser.find_element(By.XPATH, '//button[text()="4h"]').click()
+    time.sleep(4)
 
-    # Inserisci il task (modifica i selettori secondo il tuo form reale)
-    browser.fill('input[name="hours"]', '4')
-    browser.select('select[name="task_type"]', task_type.name)
-    browser.click('button[type="submit"]')  # oppure il bottone con testo 'Save' ecc.
+    # id for cell = Thu Jun 19 2025-9-456
+    #  ADD ASSERT
+    browser.assert_text('4h', selector='//td[contains(text(), "Thu Jun 19 2025")]/following-sibling::span')
 
     # Verifica che il task sia presente nella UI
-    browser.assert_text('4h', selector=f'//td[contains(text(), "{today_str}")]/following-sibling::td')
+    # browser.assert_text('4h', selector=f'//td[contains(text(), "{today_str}")]/following-sibling::td')
+
+
+#  TEST drag and drop su 1 cella, cliccare more, si apre la modale, aggiungere ore
+#  TEST drag and drop su piu' celle, cliccare 4 ed aggingere ore
+#  TEST drag and drop su 1 giorno (header),si apre la modale ed aggiungere holiday
+#  TEST drag and drop su piu' giorni, si apre la modale ed aggiungere holiday
