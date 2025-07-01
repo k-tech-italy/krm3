1782095f70bfb6f6c5cb750e0d1132ac12d26a45
know46 <krzysiek.nooworyta@gmail.com>
2025-07-01 12:26:07 +0200

    added tests for admin pannel time entries

--- tests/integration/test_admin_pannel.py
+++ tests/integration/test_admin_pannel.py
@@ -1,4 +1,3 @@
-import datetime
 import time
 
 import pytest
@@ -6,7 +5,7 @@ import pytest
 import typing
 
 from selenium.webdriver.common.by import By
-from testutils.factories import TaskFactory, TimeEntryFactory, ResourceFactory, UserFactory
+from testutils.factories import TaskFactory, TimeEntryFactory, ResourceFactory
 from django.contrib.auth.models import Permission
 
 if typing.TYPE_CHECKING:
@@ -29,13 +28,12 @@ def test_admin_should_see_all_time_entries(browser: 'AppTestBrowser', admin_user
     table_rows = browser.find_elements(By.XPATH, '//table[@id="result_list"]/tbody/tr')
     assert len(table_rows) == 2
 
-
 @pytest.mark.selenium
 @pytest.mark.django_db
 def test_staff_user_with_perms_should_see_time_all_time_entries(browser: 'AppTestBrowser', staff_user):
     resource_1 = ResourceFactory(user=staff_user)
-    permission = Permission.objects.get(codename='view_any_timesheet')
-    staff_user.user_permissions.add(permission)
+    staff_user.user_permissions.add(Permission.objects.get(codename='view_any_timesheet'),
+                                    Permission.objects.get(codename='view_timeentry'))
 
     task_1 = TaskFactory()
     task_2 = TaskFactory()
@@ -59,7 +57,7 @@ def test_staff_user_with_perms_should_see_time_all_time_entries(browser: 'AppTes
 def test_staff_user_without_perms_should_see_only_own_entries(browser: 'AppTestBrowser', staff_user):
     resource_1 = ResourceFactory(user=staff_user)
 
-    permission = Permission.objects.get(codename='view_timesheet')
+    permission = Permission.objects.get(codename='view_timeentry')
     staff_user.user_permissions.add(permission)
 
     task_1 = TaskFactory()
@@ -78,3 +76,185 @@ def test_staff_user_without_perms_should_see_only_own_entries(browser: 'AppTestB
     table_rows = browser.find_elements(By.XPATH, '//table[@id="result_list"]/tbody/tr')
     assert len(table_rows) == 1
 
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_admin_should_be_able_to_edit_any_time_entry(browser: 'AppTestBrowser', admin_user):
+    task = TaskFactory()
+    time_entry = TimeEntryFactory(task=task, day_shift_hours=4)
+
+    browser.login_as_user(admin_user)
+    browser.admin_user = admin_user
+    browser.login()
+    browser.click('//a[@href="/admin/core/timeentry/" and text() = "Time entries"]')
+    browser.click(f'//a[@href="/admin/core/timeentry/{time_entry.id}/change/"]')
+
+    day_shift_input = browser.find_element(By.XPATH, '//input[@name="day_shift_hours"]')
+    day_shift_input.clear()
+    day_shift_input.send_keys('7')
+
+    browser.click('//input[@value="Save"]')
+    browser.click(f'//a[@href="/admin/core/timeentry/{time_entry.id}/change/"]')
+
+    browser.assert_element('//input[@name="day_shift_hours" and @value="7.00"]')
+
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_staff_user_without_manage_any_timesheet_perm_should_be_able_to_edit_only_owned_time_entry(
+        browser: 'AppTestBrowser', staff_user):
+    resource = ResourceFactory(user=staff_user)
+
+    staff_user.user_permissions.add(Permission.objects.get(codename='view_any_timesheet'),
+                                    Permission.objects.get(codename='view_timeentry'))
+
+    task_1 = TaskFactory()
+    owned_time_entry = TimeEntryFactory(task=task_1, day_shift_hours=4, resource=resource)
+
+    task_2 = TaskFactory()
+    not_owned_time_entry = TimeEntryFactory(task=task_2, day_shift_hours=5)
+
+    browser.login_as_user(staff_user)
+    browser.admin_user = staff_user
+    browser.login()
+
+    browser.click('//a[@href="/admin/core/timeentry/" and text() = "Time entries"]')
+
+    browser.click(f'//a[@href="/admin/core/timeentry/{not_owned_time_entry.id}/change/"]')
+    time.sleep(2)
+    browser.assert_element_absent('//input[@name="day_shift_hours"]')
+
+    browser.click('//a[@href="/admin/core/timeentry/" and text() = "Time entries"]')
+    browser.click(f'//a[@href="/admin/core/timeentry/{owned_time_entry.id}/change/"]')
+
+    day_shift_input = browser.find_element(By.XPATH, '//input[@name="day_shift_hours"]')
+    day_shift_input.clear()
+    day_shift_input.send_keys('3')
+
+    browser.click('//input[@value="Save"]')
+    browser.click(f'//a[@href="/admin/core/timeentry/{owned_time_entry.id}/change/"]')
+
+    browser.assert_element('//input[@name="day_shift_hours" and @value="3.00"]')
+
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_staff_user_with_manage_any_timesheet_perm_should_be_able_to_edit_any_time_entry(
+        browser: 'AppTestBrowser', staff_user):
+
+    staff_user.user_permissions.add(Permission.objects.get(codename='manage_any_timesheet'),
+                                    Permission.objects.get(codename='change_timeentry'))
+    task = TaskFactory()
+    time_entry = TimeEntryFactory(task=task, day_shift_hours=4)
+
+    browser.login_as_user(staff_user)
+    browser.admin_user = staff_user
+    browser.login()
+    browser.click('//a[@href="/admin/core/timeentry/" and text() = "Time entries"]')
+    browser.click(f'//a[@href="/admin/core/timeentry/{time_entry.id}/change/"]')
+
+    day_shift_input = browser.find_element(By.XPATH, '//input[@name="day_shift_hours"]')
+    day_shift_input.clear()
+    day_shift_input.send_keys('2')
+
+    browser.click('//input[@value="Save"]')
+    browser.click(f'//a[@href="/admin/core/timeentry/{time_entry.id}/change/"]')
+
+    browser.assert_element('//input[@name="day_shift_hours" and @value="2.00"]')
+
+
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_admin_should_be_able_to_add_time_entry_for_any_resource(browser: 'AppTestBrowser', admin_user):
+    task = TaskFactory()
+
+    ResourceFactory(user=admin_user)
+
+    browser.login_as_user(admin_user)
+    browser.admin_user = admin_user
+    browser.login()
+
+    browser.click('//a[@href="/admin/core/timeentry/add/"]')
+
+    day_shift_input = browser.find_element(By.XPATH, '//input[@name="day_shift_hours"]')
+    day_shift_input.clear()
+    day_shift_input.send_keys('2')
+    browser.click('//p/span/a[@href="#" and text()="Today"]')
+    browser.click('//select[@id="id_task"]')
+
+    browser.click(f'//option[contains(text(), "{task.title}")]')
+
+    browser.click('//select[@id="id_resource"]')
+
+    resource_options = browser.find_elements(By.XPATH, '//select[@id="id_resource"]/option')
+
+    assert len(resource_options) == 3
+
+    browser.click(f'//option[contains(text(), "{task.resource.last_name}")]')
+
+    browser.click('//input[@value="Save"]')
+    browser.assert_element('//li[@class="success"]')
+
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_staff_user_with_manage_any_timesheet_perm_should_be_able_to_add_time_entry_for_any_resource(
+        browser: 'AppTestBrowser', staff_user):
+    staff_user.user_permissions.add(Permission.objects.get(codename='manage_any_timesheet'),
+                                    Permission.objects.get(codename='view_timeentry'))
+    task = TaskFactory()
+
+    ResourceFactory(user=staff_user)
+
+    browser.login_as_user(staff_user)
+    browser.admin_user = staff_user
+    browser.login()
+
+    browser.click('//a[@href="/admin/core/timeentry/add/"]')
+
+    day_shift_input = browser.find_element(By.XPATH, '//input[@name="day_shift_hours"]')
+    day_shift_input.clear()
+    day_shift_input.send_keys('2')
+    browser.click('//p/span/a[@href="#" and text()="Today"]')
+    browser.click('//select[@id="id_task"]')
+
+    browser.click(f'//option[contains(text(), "{task.title}")]')
+
+    browser.click('//select[@id="id_resource"]')
+
+    resource_options = browser.find_elements(By.XPATH, '//select[@id="id_resource"]/option')
+    assert len(resource_options) == 3
+
+    browser.click(f'//option[contains(text(), "{task.resource.last_name}")]')
+
+    browser.click('//input[@value="Save"]')
+    browser.assert_element('//li[@class="success"]')
+
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_staff_user_without_manage_any_timesheet_perm_should_be_able_to_add_time_entry_only_for_owned_resource(
+        browser: 'AppTestBrowser', staff_user):
+    staff_user.user_permissions.add(Permission.objects.get(codename='add_timeentry'))
+
+    owned_resource = ResourceFactory(user=staff_user)
+    task = TaskFactory(resource=owned_resource)
+
+    browser.login_as_user(staff_user)
+    browser.admin_user = staff_user
+    browser.login()
+
+    browser.click('//a[@href="/admin/core/timeentry/add/"]')
+
+    day_shift_input = browser.find_element(By.XPATH, '//input[@name="day_shift_hours"]')
+    day_shift_input.clear()
+    day_shift_input.send_keys('2')
+    browser.click('//p/span/a[@href="#" and text()="Today"]')
+    browser.click('//select[@id="id_task"]')
+
+    browser.click(f'//option[contains(text(), "{task.title}")]')
+
+    browser.click('//select[@id="id_resource"]')
+
+    resource_options = browser.find_elements(By.XPATH, '//select[@id="id_resource"]/option')
+    assert len(resource_options) == 2
+
+    browser.click(f'//option[contains(text(), "{owned_resource.last_name}")]')
+
+    browser.click('//input[@value="Save"]')
+    browser.assert_element('//li[@class="success"]')
--- tests/integration/timesheet/test_calendar.py
+++ tests/integration/timesheet/test_calendar.py
@@ -39,32 +39,33 @@ def test_entries_exceed_24h(browser: 'AppTestBrowser', regular_user, resource_fa
     browser.click('[href*="/timesheet"]')
     time.sleep(2)
 
-    entry_tile_1 = browser.find_element('xpath','//div[contains(@id, "draggable-Mon Jun 02")]')
+    entry_tile_1 = browser.find_element('xpath','//div[contains(@id, "Mon Jun 02")]')
     actions = ActionChains(browser.driver)
 
     actions.click_and_hold(entry_tile_1).pause(1).release().perform()
 
     browser.click('//*[contains(text(), "More")]')
+
     browser.fill('//input[@id="daytime-input"]', '14')
     browser.click('//button/span[contains(text(), "Save")]')
 
     time.sleep(2)
-    entry_tile_2 = browser.find_element('xpath','//div[contains(@id, "draggable-Mon Jun 02")]')
+
+    entry_tile_2 = browser.find_elements('xpath','//div[starts-with(@id, "Mon Jun 02")]')[1]
     actions.click_and_hold(entry_tile_2).pause(1).release().perform()
+
     browser.click('//*[contains(text(), "More")]')
     browser.fill('//input[@id="daytime-input"]', '13')
+
     browser.click('//button/span[contains(text(), "Save")]')
 
-    browser.assert_element(
-        '//p[@id="creation-error-message" '
-        'and text()="Invalid time entry for 2025-06-02: '
-        'Total hours on all time entries on 2025-06-02 (27.00) is over 24 hours."]')
-
+    browser.assert_element('//p[@id="creation-error-message" and text()="Invalid time entry for 2025-06-02: '
+                           'Total hours on all time entries on 2025-06-02 (27.00) is over 24 hours."]')
 
     browser.click('//button[@aria-label="Close modal"]')
 
-    browser.assert_element('//div[contains(@id, "draggable")]//span[contains(text(), "14")]')
-    browser.assert_element_absent('//div[contains(@id, "draggable")]//span[contains(text(), "13")]')
+    browser.assert_element('//div[contains(@id, "Mon Jun 02 2025")]//span[contains(text(), "14")]')
+    browser.assert_element_absent('//div[contains(@id, "Mon Jun 02 2025")]//span[contains(text(), "13")]')
 
 
 @freeze_time('2025-05-13')
@@ -108,13 +109,12 @@ def test_display_multiple_tasks(browser: 'AppTestBrowser', regular_user, freeze_
     browser.assert_element(f'//div[div[div[text()="{task_3.title}"]] and following-sibling::div[text()="8"]]')
 
     # check total hours for days
-    browser.assert_element('//div[@id="draggable-column-20"]/div/div/div/div[contains(., "14h")]')
-    browser.assert_element('//div[@id="draggable-column-21"]/div/div/div/div[contains(., "1h")]')
+    browser.assert_element('//div[@id="column-20"]/div/div/div/div[contains(., "14h")]')
+    browser.assert_element('//div[@id="column-21"]/div/div/div/div[contains(., "1h")]')
 
     # check if edit menu is being opened
-    entry_tile = browser.find_element('xpath', '//div[contains(@id, "draggable-Fri May 02")]')
+    entry_tile = browser.find_element('xpath', '//div[contains(@id, "Fri May 02")]')
     actions = ActionChains(browser.driver)
     actions.click_and_hold(entry_tile).pause(1).release().perform()
     browser.click('//*[contains(text(), "More")]')
     browser.assert_element('//button/span[contains(text(), "Save")]')
-
