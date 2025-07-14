b895609984b6fac191b26e1d298574b96ec68194
Raffaele Tosti <r.tosti@k-tech.it>
2025-07-14 15:16:19 +0200

    Task #150 API for report tables (#45)

--- src/krm3/timesheet/api/urls.py
+++ src/krm3/timesheet/api/urls.py
@@ -9,6 +9,6 @@ router.register('', api_views.TimesheetAPIViewSet, basename='api-timesheet')
 
 router.register('time-entry', api_views.TimeEntryAPIViewSet, basename='api-time-entry')
 router.register('special-leave-reason', api_views.SpecialLeaveReasonViewSet, basename='api-special-leave-reason')
-router.register('report/export', api_views.ReportViewSet, basename='api-report')
+router.register('report', api_views.ReportViewSet, basename='api-report')
 
 urlpatterns = router.urls
