aa7b9d20fd19d6e7acfc9d33e3206398e94582e7
know46 <krzysiek.nooworyta@gmail.com>
2025-07-22 09:48:06 +0200

    Added backend home page

--- src/krm3/web/urls.py
+++ src/krm3/web/urls.py
@@ -4,6 +4,8 @@ from .views import HomeView, ExampleView
 
 urlpatterns = [
     path('example/', ExampleView.as_view(), name='home'),
-    path('', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view()),
+    path('home/', HomeView.as_view(), name='home'),
     path('missions/', include('krm3.missions.urls'))
+
 ]
