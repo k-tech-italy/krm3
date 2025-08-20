ac9e6203866df3ed14a55e482eb0e0226fda8809
Kuba <kubaa.rutkowski123@gmail.com>
2025-08-20 14:34:53 +0200

    Add tests for report and task_report view

--- tests/unit/web/test_views.py
+++ tests/unit/web/test_views.py
@@ -9,18 +9,20 @@ from testutils.factories import (
     ResourceFactory,
     TimeEntryFactory,
     SpecialLeaveReasonFactory,
+    TaskFactory
 )
 from urllib.parse import urlencode
 
 from freezegun import freeze_time
 
 
+
 def _assert_homepage_content(response):
     assert response.status_code == 200
     content = response.content.decode()
 
-    report_expected_url = reverse('admin:core_timeentry_report')
-    task_report_expected_url = reverse('admin:core_timeentry_task_report')
+    report_expected_url = reverse('report')
+    task_report_expected_url = reverse('task_report')
     availability_report_expected_url = reverse('availability')
 
     assert 'Report' in content
@@ -59,7 +61,7 @@ def test_superuser_should_see_homepage(client):
     _assert_homepage_content(response)
 
 
-@pytest.mark.parametrize('url', ('/be/home/', '/be/', '/be/availability/'))
+@pytest.mark.parametrize('url', ('/be/home/', '/be/', '/be/availability/', '/be/report/', '/be/task_report/'))
 def test_user_without_permissions_should_not_see_web_urls(url, client):
     UserFactory(username='user00', password='pass123')
     client.login(username='user00', password='pass123')
@@ -67,7 +69,7 @@ def test_user_without_permissions_should_not_see_web_urls(url, client):
     assert response.status_code == 403
 
 
-@pytest.mark.parametrize('url', ('/be/home/', '/be/', '/be/availability/'))
+@pytest.mark.parametrize('url', ('/be/home/', '/be/', '/be/availability/', '/be/report/', '/be/task_report/'))
 def test_not_authenticated_user_should_be_redirected_to_login_page(client, url):
     response = client.get(url)
     assert response.status_code == 302
@@ -122,6 +124,95 @@ def test_availability_view_next_previous_month(client, month, expected_result):
     SuperUserFactory(username='user00', password='pass123')
     client.login(username='user00', password='pass123')
     response = client.get(f'{reverse('availability')}?{urlencode({"month": month})}')
+    _assert_homepage_content(response)
+    assert response.status_code == 200
+    assert (
+        f'<h1 class="text-3xl font-bold text-center mb-1">{expected_result}</h1>'
+        in response.content.decode()
+    )
+
+@freeze_time('2025-08-22')
+def test_report_view_current_month(client):
+    SuperUserFactory(username='user00', password='pass123')
+    resource = ResourceFactory()
+    TimeEntryFactory(
+        resource=resource,
+        day_shift_hours=0,
+        date=datetime.date.today(),
+        holiday_hours=8,
+    )
+    client.login(username='user00', password='pass123')
+    url = reverse('report')
+    response = client.get(url)
+    _assert_homepage_content(response)
+    assert response.status_code == 200
+    content = response.content.decode()
+    assert (
+        f'{resource.first_name} {resource.last_name}'
+        in content
+    )
+    assert '<td class="p-1 border border-1 text-center">8</td>' in content
+    assert (
+        '<h1 class="text-3xl font-bold text-center mb-1">Report August 2025</h1>'
+        in content
+    )
+
+@freeze_time('2025-08-22')
+@pytest.mark.parametrize(
+    'month, expected_result',
+    [
+        pytest.param('202509', 'Report September 2025', id='next_month'),
+        pytest.param('202507', 'Report July 2025', id='previous_month'),
+    ],
+)
+def test_report_view_next_previous_month(client, month, expected_result):
+    SuperUserFactory(username='user00', password='pass123')
+    client.login(username='user00', password='pass123')
+    response = client.get(f'{reverse('report')}?{urlencode({"month": month})}')
+    _assert_homepage_content(response)
+    assert response.status_code == 200
+    assert (
+        f'<h1 class="text-3xl font-bold text-center mb-1">{expected_result}</h1>'
+        in response.content.decode()
+    )
+
+@freeze_time('2025-08-22')
+def test_task_report_view_current_month(client):
+    SuperUserFactory(username='user00', password='pass123')
+    task = TaskFactory()
+    TimeEntryFactory(
+        resource=task.resource,
+        day_shift_hours=8,
+        date=datetime.date.today(),
+        task=task
+    )
+    client.login(username='user00', password='pass123')
+    url = reverse('task_report')
+    response = client.get(url)
+    _assert_homepage_content(response)
+    assert response.status_code == 200
+    content = response.content.decode()
+    assert (
+        f'<td class="border border-1 text-left p-1 ">{task.project}: {task.title}</td>'
+        in content
+    )
+    assert (
+        '<h1 class="text-3xl font-bold text-center mb-1">Task Report August 2025</h1>'
+        in content
+    )
+
+@freeze_time('2025-08-22')
+@pytest.mark.parametrize(
+    'month, expected_result',
+    [
+        pytest.param('202509', 'Task Report September 2025', id='next_month'),
+        pytest.param('202507', 'Task Report July 2025', id='previous_month'),
+    ],
+)
+def test_task_report_view_next_previous_month(client, month, expected_result):
+    SuperUserFactory(username='user00', password='pass123')
+    client.login(username='user00', password='pass123')
+    response = client.get(f'{reverse('task_report')}?{urlencode({"month": month})}')
     _assert_homepage_content(response)
     assert response.status_code == 200
     assert (
