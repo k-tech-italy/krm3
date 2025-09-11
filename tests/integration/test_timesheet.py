a621d4bc95ae0bbd13ef667fec1dd54cbfd2b76c
dacorsa <36949692+dacorsa@users.noreply.github.com>
2025-09-11 14:40:02 +0200

    test_save_bank_hours_after_use_hours

--- tests/integration/test_timesheet.py
+++ tests/integration/test_timesheet.py
@@ -391,3 +391,52 @@ def test_add_bank_hours_below_scheduled_hours(
     actual_error = error_element.text.strip()
     
     assert actual_error == expected_error, f"❌ Messaggio d'errore inatteso: {actual_error}"
+    
+@freeze_time('2025-07-13')
+def test_save_bank_hours_after_use_hours(
+        browser: 'AppTestBrowser', regular_user, freeze_frontend_time):
+
+    freeze_frontend_time('2025-07-13T00:00:00Z')
+    resource = ResourceFactory(user=regular_user)
+    
+    TimeEntryFactory(resource=resource,task=TaskFactory(resource=resource), day_shift_hours=6, date='2025-07-04')
+
+    browser.login_as_user(regular_user)
+    browser.click('[href*="timesheet"]')
+    
+    browser.assert_element('//p[@data-testid="bank-total" and contains(text(), "0")]')
+    bank_delta = browser.find_element('//p[@data-testid="bank-delta"]')
+    assert bank_delta.text.strip() == '(𝚫 = +0h)'
+
+    day_tile = browser.wait_for_element_visible('//div[contains(@data-testid, "header-2025-07-04")]')
+    browser.click_and_release(day_tile)
+
+    browser.fill('//input[contains(@id,"from-bank-hour-input")]', '2')   
+    browser.click('//button[contains(text(), "Save")]')
+    
+    browser.assert_element('//p[@data-testid="bank-total" and contains(text(), "0")]')
+    bank_delta = browser.find_element('//p[@data-testid="bank-delta"]')
+    assert bank_delta.text.strip() == '(𝚫 = -2h)'
+
+    day_tile = browser.wait_for_element_visible('//div[contains(@data-testid, "header-2025-07-04")]')
+    browser.click_and_release(day_tile)
+
+    browser.fill('//input[contains(@id,"save-bank-hour-input")]', '2')
+    browser.click('//button[contains(text(), "Save")]')
+
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+    driver = browser.driver
+    wait = WebDriverWait(driver, 10)
+
+    error_element = wait.until(
+        EC.visibility_of_element_located((By.ID, "creation-error-message"))
+    )
+
+    expected_error = "Invalid time entry for 2025-07-04: Cannot both withdraw from and deposit to bank hours on the same day."
+    actual_error = error_element.text.strip()
+    
+    assert actual_error == expected_error, f"❌ Messaggio d'errore inatteso: {actual_error}"
+    
