8a38ff8220ee2d62e29103a7e9cb7c663c50cff1
GigiusB <g.bronzini@gmail.com>
2025-09-17 12:41:13 +0100

    fix(Trying): Trying

--- src/krm3/missions/media.py
+++ src/krm3/missions/media.py
@@ -1,8 +1,11 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from krm3.core.models import Mission
+
 EXPENSES_IMAGE_PREFIX = 'missions/expenses'
 
 
-def mission_directory_path(instance, filename):
+def mission_directory_path(instance: 'Mission', filename: str) -> str:
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return '{0}/R{1}/M{2}/{3}'.format(
-        EXPENSES_IMAGE_PREFIX, instance.mission.resource.id, instance.mission.id, filename
-    )
+    return '{EXPENSES_IMAGE_PREFIX}/R{instance.mission.resource.id}/M{instance.mission.id}/{filename}'
