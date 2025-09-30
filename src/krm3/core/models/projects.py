733e8952b91c01a340d30c4a5ddd8988a3ad776d
Kuba <kubaa.rutkowski123@gmail.com>
2025-09-30 16:42:02 +0200

    fix(admin.py): set start_date mandatory for project and provide initial value in ProjectForm

--- src/krm3/core/models/projects.py
+++ src/krm3/core/models/projects.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import datetime
 from typing import TYPE_CHECKING, Self, override, Iterable
 
 from django.core.exceptions import ValidationError
@@ -13,6 +12,7 @@ from .contacts import Client
 from .timesheets import TimeEntry
 
 if TYPE_CHECKING:
+    import datetime
     from django.db.models.base import ModelBase
     from decimal import Decimal
     from krm3.core.models.auth import User
@@ -35,7 +35,7 @@ class ProjectManager(NaturalKeyModelManager):
 class Project(NaturalKeyModel):
     name = models.CharField(max_length=80, unique=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    start_date = models.DateField(null=True, blank=True)
+    start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     metadata = models.JSONField(default=dict, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
@@ -62,8 +62,6 @@ class Project(NaturalKeyModel):
         using: str | None = None,
         update_fields: Iterable[str] | None = None,
     ) -> None:
-        if self.start_date is None:
-            self.start_date = datetime.date.today()
         self.full_clean()
         super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
