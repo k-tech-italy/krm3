253c8fd57137857292e576b435036fc90bbed13c
Giovanni B. <g.bronzini@gmail.com>
2025-12-02 15:43:20 +0000

    Fixes #482 (#178)

--- src/krm3/utils/threading.py
+++ src/krm3/utils/threading.py
@@ -1,7 +1,10 @@
 """Threading admin panel module."""
 
+from __future__ import annotations
+
 import logging
 import time
+import typing
 from functools import partial
 from signal import SIGTSTP, pthread_kill
 from threading import Thread
@@ -9,12 +12,12 @@ from threading import Thread
 logger = logging.getLogger(__name__)
 
 
-def wait_for_tasks(tasks: list[Thread], timeout=10):  # noqa: D103
+def wait_for_tasks(tasks: list[Thread], timeout: int = 10) -> None:  # noqa: D103
     start = time.time()
     for p in tasks:
         p.start()
     while time.time() - start <= timeout:
-        if not any([p.is_alive() for p in tasks]):
+        if not any(p.is_alive() for p in tasks):
             break
         time.sleep(0.1)  # Just to avoid hogging the CPU
     else:
@@ -24,12 +27,14 @@ def wait_for_tasks(tasks: list[Thread], timeout=10):  # noqa: D103
                 pthread_kill(p.ident, SIGTSTP)
 
 
-def runs_in_background(args, timeout=10):  # noqa: D103
+def runs_in_background(args: typing.Iterable, timeout: int = 10) -> list:  # noqa: D103
     return [run_in_background(t, a, k, timeout, False) for t, a, k in args]
 
 
-def run_in_background(func, args=None, kwargs=None, timeout=10, join=True):  # noqa: D103
-    def _run(f, *a, **kw):
+def run_in_background(
+    func: typing.Callable, args: typing.Iterable = None, kwargs: dict = None, timeout: int = 10, join: bool = True
+) -> Thread | None:  # noqa: D103
+    def _run(f: typing.Callable, *a: typing.ParamSpecArgs, **kw: typing.ParamSpecKwargs) -> None:
         try:
             f(*a, **kw)
         except Exception as e:
@@ -38,5 +43,5 @@ def run_in_background(func, args=None, kwargs=None, timeout=10, join=True):  # n
     t = Thread(target=partial(_run, func), args=args or [], kwargs=kwargs or {})
     if join:
         wait_for_tasks([t], timeout)
-    else:
-        return t
+        return None
+    return t
