a31c14143fafb59daa7668804380a9235dbcab14
know46 <krzysiek.nooworyta@gmail.com>
2025-12-03 11:18:58 +0100

    fix(utils.py): fix get_resource_timesheet to return correct data

--- src/krm3/timesheet/utils.py
+++ src/krm3/timesheet/utils.py
@@ -3,27 +3,19 @@ from __future__ import annotations
 from decimal import Decimal
 from typing import TYPE_CHECKING
 
-from dateutil.relativedelta import relativedelta
 
-from krm3.core.models import Resource, TimeEntry, User, TimesheetSubmission
 from krm3.timesheet.dto import TimesheetDTO
 
 if TYPE_CHECKING:
     import datetime
     from collections.abc import Iterable
+    from krm3.core.models import Resource, TimeEntry, User
 
 
 def get_resource_timesheet(
     end_date: datetime.date, resource: Resource, start_date: datetime.date, requestor: User
 ) -> TimesheetDTO:
     """Retrieve the resource timesheet for a specific date interval."""
-    tms = TimesheetSubmission.objects.filter(
-        resource=resource, period=[start_date, end_date + relativedelta(days=1)]
-    ).first()
-
-    if tms and tms.closed and tms.timesheet:
-        return tms.timesheet
-
     return TimesheetDTO(requested_by=requestor).fetch(resource, start_date, end_date)
 
 
