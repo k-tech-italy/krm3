0d96860c01fc8864f15cabc2534877164cfc9a3d
dpaolicelli-ktech <d.paolicelli@k-tech.it>
2026-01-19 14:30:54 +0100

    fix(events): configure events from environment variables

--- src/krm3/config/fragments/events.py
+++ src/krm3/config/fragments/events.py
@@ -1,6 +1,8 @@
+from krm3.config.environ import env
+
 __all__ = ['EVENTS']
 
 EVENTS = {
-    'BACKEND': 'krm3.events.backends.NullEventDispatcherBackend',
-    'OPTIONS': {},
+    'BACKEND': env('EVENT_DISPATCHER_BACKEND'),
+    'OPTIONS': env('EVENT_DISPATCHER_OPTIONS'),
 }
