f03f2a9aadce0ecc545eb54cacaa11f90c77e6d6
Gabi <g.stepien@k-tech.it>
2026-01-26 16:12:52 +0000

    middleware test

--- tests/unit/middlewares/test_language_selection.py
+++ tests/unit/middlewares/test_language_selection.py
@@ -9,9 +9,19 @@ User = get_user_model()
 @pytest.mark.db
 def test_preferred_language_persists_across_sessions(django_client_and_auth_resource):
     client, resource = django_client_and_auth_resource
+    language_cookie_name = settings.LANGUAGE_COOKIE_NAME
     response = client.get('/')
 
-    session_language = response.cookies.get(settings.LANGUAGE_COOKIE_NAME)
-    assert resource.preferred_language == 'it', "Sanity check, ResourceFactory should be setting this."
-    assert session_language.coded_value == 'it', "Middleware should set the cookie."
+    session_language = response.cookies.get(language_cookie_name)
+    assert resource.preferred_language == 'pl', "Sanity check, ResourceFactory should be setting this."
+    assert session_language.coded_value == 'pl', "Middleware should set the cookie."
+
+    client.logout()
+    client.force_login(resource.user)
+
+    post_re_login_response = client.get('/admin/')
+    post_re_login_response_session_language = post_re_login_response.cookies.get(language_cookie_name)
+    assert settings.LANGUAGE_CODE == 'en-uk', "Check we are not false-passing by checking default language."
+    assert post_re_login_response_session_language.coded_value == 'pl', "Middleware should set the cookie."
+    assert "Witaj" in post_re_login_response.content.decode()
 
