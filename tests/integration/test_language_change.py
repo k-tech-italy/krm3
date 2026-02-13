fabe23f0b0638a0eeb817297eab8e5606043f053
Gabi <g.stepien@k-tech.it>
2026-02-13 17:08:15 +0000

    improve test

--- tests/integration/test_language_change.py
+++ tests/integration/test_language_change.py
@@ -1,49 +1,45 @@
 import pytest
 from django.shortcuts import reverse
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.mark.selenium
 def test_user_selects_preferred_language(browser, resource):
     browser.login_as_user(resource.user)
 
-    # Navigate to the page
-    browser.get(reverse('user_resource', args=[resource.user.id]))
+    # Wait for login to complete by checking for the session cookie
+    WebDriverWait(browser, 10).until(
+        lambda driver: any(cookie['name'] == 'sessionid' for cookie in driver.get_cookies())
+    )
+    browser.driver.get(str(browser.live_server_url) + reverse('user_resource', args=[resource.user.id]))
 
-    # Find the language dropdown by its ID
-    language_dropdown = browser.find_element('id', 'id_preferred_language')
+    # Wait for the language dropdown to be present and interactable
+    language_dropdown = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable(('id', 'id_preferred_language'))
+    )
 
-    # Change language to 'fr' via the dropdown
-    from selenium.webdriver.support.ui import Select
     select = Select(language_dropdown)
-    select.select_by_value('fr')
+    select.select_by_value('it')
 
-    # Submit the form
     submit_button = browser.find_element('css selector', 'button[type="submit"]')
     submit_button.click()
 
-    # Wait for the page to reload/update
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located(('id', 'id_preferred_language'))
     )
 
-    # Assert that the selected language in dropdown is 'fr'
     language_dropdown = browser.find_element('id', 'id_preferred_language')
     select = Select(language_dropdown)
-    assert select.first_selected_option.get_attribute('value') == 'fr'
+    assert select.first_selected_option.get_attribute('value') == 'it'
 
-    # Assert that in the cookies we now have "django_language" with value "fr"
     cookies = browser.get_cookies()
     django_language_cookie = next(
         (cookie for cookie in cookies if cookie['name'] == 'django_language'),
         None
     )
     assert django_language_cookie is not None
-    assert django_language_cookie['value'] == 'fr'
-#
-#
-#
-# @pytest.mark.selenium
-# def test_user_changes_via_quick_preferred_language(browser):
-#     pass
+    assert django_language_cookie['value'] == 'it'
+
+    browser.driver.get(str(browser.live_server_url) + '/')
+    assert "Benvenuta" in browser.get_page_source()
