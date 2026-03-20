6181ba45366276199539cb38c6b16104a66bfa57
GigiusB <g.bronzini@gmail.com>
2026-03-20 07:59:59 +0000

    Improved tools/dev/techdb.sh

--- tools/dev/fetchdb.sh
+++ tools/dev/fetchdb.sh
@@ -7,11 +7,15 @@ DB_PORT=`python manage.py shell --no-imports -c "from django.conf import setting
 DB_NAME=`python manage.py shell --no-imports -c "from django.conf import settings; print(settings.DATABASES['default']['NAME'], end='')"`
 echo "Initialising DB ${DB_NAME}"
 
-echo Deleting database "${DB_NAME}"
-psql -h ${DB_HOST} -p ${DB_PORT} -U postgres -c "DROP DATABASE IF EXISTS ${DB_NAME}"
-psql -h ${DB_HOST} -p ${DB_PORT} -U postgres -c "CREATE DATABASE ${DB_NAME}"
+psql -h localhost -p ${REMOTE_DB_PORT:-16432} -U postgres -d krm3 -c "SELECT 1"
 
 if [ "$?" = "0" ]; then
-  pg_dump -p ${REMOTE_DB_PORT:-16432} -h localhost -U postgres -d krm3 -O -x | grep -v "transaction_timeout" | psql -h localhost -p ${DB_PORT} -U postgres --set ON_ERROR_STOP=on -d ${DB_NAME}
+  echo "Deleting local database ${DB_NAME}"
+  psql -h ${DB_HOST} -p ${DB_PORT} -U postgres -c "DROP DATABASE IF EXISTS ${DB_NAME}"
+  psql -h ${DB_HOST} -p ${DB_PORT} -U postgres -c "CREATE DATABASE ${DB_NAME}"
+
+  pg_dump -p ${REMOTE_DB_PORT:-16432} -h localhost -U postgres -d krm3 -O -x | grep -v "transaction_timeout" | psql -h localhost -p ${DB_PORT} -U postgres --set ON_ERROR_STOP=on -d ${DB_NAME} && \
   DJANGO_SUPERUSER_PASSWORD=123 ./manage.py createsuperuser --username admin --email a@a.com --noinput
+else
+  echo "Error: could not contact remote DB on localhost:${REMOTE_DB_PORT:-16432}"
 fi
