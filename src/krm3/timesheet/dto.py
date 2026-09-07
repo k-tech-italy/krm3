14b5b4fec3a5f7c3c25c914e5b8d1e25ae85d838
Kuba <kubaa.rutkowski123@gmail.com>
2026-09-07 16:25:28 +0200

    fix(dto): adapt DTO to new DayEntry model

--- src/krm3/timesheet/dto.py
+++ src/krm3/timesheet/dto.py
@@ -2,17 +2,17 @@
 
 import datetime
 import typing
-from typing import Self
+from typing import Self, cast
 
 from constance import config
 from django.contrib.postgres.fields import ranges
+from ktcalendars import KTDay
+from psycopg.types.range import DateRange
 
 from krm3.core.models.auth import Resource, User
 from krm3.core.models.contracts import Contract
 from krm3.core.models.projects import Task, TaskQuerySet
-from krm3.core.models.timesheets import TaskEntry, TaskEntriesQuerySet, DayEntry, DayEntriesQuerySet
-from ktcalendars import KTCalendar
-from dateutil.relativedelta import relativedelta
+from krm3.core.models.timesheets import DayEntriesQuerySet, DayEntry
 
 if typing.TYPE_CHECKING:
     from krm3.config.fragments.constance import ConstanceTyping
@@ -24,33 +24,39 @@ class TimesheetDTO:
         self.day_entries = DayEntriesQuerySet().none()
         self.requested_by = requested_by
         self.resource = None
+        self.schedule = {}
         self.timesheet_colors = {}
         self.bank_hours = 0.0
+        self.days = []
         self.contracts = Contract.objects.none()
 
     def fetch(self, resource: Resource, start_date: datetime.date, end_date: datetime.date) -> Self:
         """Fetch the resource timesheet for a specific date interval."""
-        upper = end_date + relativedelta(days=1)
-        self.tasks = Task.objects.filter(period__overlap=(start_date, upper)).assigned_to(
-            resource=resource
-        )
-        if self.requested_by:
-            self.tasks = self.tasks.filter_acl(user=self.requested_by)
+        task_qs = Task.objects.filter_acl(self.requested_by) if self.requested_by else Task.objects.all()
+        self.tasks = cast(
+            'TaskQuerySet',
+            (task_qs.filter(period__overlap=DateRange(start_date, end_date))),
+        ).assigned_to(resource=resource)
 
-        self.day_entries = DayEntry.objects.filter(
-            day__gte=start_date, day__lte=end_date
-        ).prefetch_related('taskentry_set').select_related('contract')
-        if self.requested_by:
-            self.day_entries = self.day_entries.filter_acl(user=self.requested_by)
+        te_qs = DayEntry.objects.filter_acl(self.requested_by) if self.requested_by else DayEntry.objects.all()
+        self.day_entries = te_qs.filter(resource=resource, day__range=(start_date, end_date))
 
         self.contracts = Contract.objects.filter(
-            resource=resource, period__overlap=ranges.DateRange(start_date, upper)
+            resource=resource,
+            period__overlap=ranges.DateRange(start_date, end_date + datetime.timedelta(days=1)),
         )
-        if self.requested_by:
-            self.contracts = self.contracts.filter_acl(user=self.requested_by)
 
-        self.days = list(KTCalendar().iter_dates(start_date, end_date))
         self.resource = resource
+
+        contract_map = resource.get_contract_map(start_date, end_date)
+        for period, contract in sorted(contract_map.items()):
+            for day in period:
+                if contract is None:
+                    self.days.append(KTDay(day))
+                    self.schedule[day] = 0.0
+                else:
+                    self.days.append(contract.get_ktday(day))
+                    self.schedule[day] = float(contract.get_due_hours(day))
 
         conf: ConstanceTyping = config
         self.timesheet_colors.update(
@@ -64,6 +70,6 @@ class TimesheetDTO:
             }
         )
 
-        self.bank_hours = resource.get_bank_hours_balance(at=end_date)
+        self.bank_hours = resource.get_bank_hours_balance(end_date)
 
         return self
